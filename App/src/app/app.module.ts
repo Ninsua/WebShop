29fae99b78da1c6285a3d26e47b9dbc2b56a0289
Cristian B <35538886+cristianmtb@users.noreply.github.com>
2018-04-24 15:41:30 +0200

    Added admin panel component

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    AdminPanelComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
