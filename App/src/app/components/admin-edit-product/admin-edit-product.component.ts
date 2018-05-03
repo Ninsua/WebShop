7993635b1025fd796252d650da79b09b2ec27e70
Ninsua <e@anjel.se>
2018-05-03 16:43:57 +0200

    Fixed bugs, applied style, added imagePath to product class, implemented delete of old images

--- App/src/app/components/admin-edit-product/admin-edit-product.component.ts
+++ App/src/app/components/admin-edit-product/admin-edit-product.component.ts
@@ -6,6 +6,7 @@ import { Product, Category } from '../../classes/classes';
 import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage'; 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import * as firebase from 'firebase/app';
 
 @Component({
 	selector: 'app-admin-edit-product',
@@ -34,6 +35,8 @@ export class AdminEditProductComponent implements OnInit {
 	snapshot: Observable<any>;
 	// Download URL
 	downloadURL: Observable<string>;
+	oldImage: string="";
+	changedImage:boolean=false;
 	
 	constructor(
 		private route: ActivatedRoute,
@@ -67,7 +70,12 @@ export class AdminEditProductComponent implements OnInit {
     }
 		
     // The storage path
-    const path = 'Products/image/'+(new Date().getTime())+'_'+file.name;
+    const path = 'Products/images/'+(new Date().getTime())+'_'+file.name;
+	//Remove the old image path and add the new image path 
+	this.oldImage = this.product.imagePath;
+	this.product.imagePath = path;
+	this.changedImage=true;
+	
     // The main task
     this.task = this.storage.upload(path, file);
     // progress monitoring 
@@ -85,6 +93,7 @@ export class AdminEditProductComponent implements OnInit {
 	onSubmit() {
 		const itemRef = this.db.object('/Products/'+this.key);
 		
+		//Checks if content is missing, else update in database
 		if (this.product.name.length == 0 || isNaN(this.product.price) || isNaN(this.product.numInStock) || this.product.description.length == 0) {
 			alert('Some properties are missing!');
 		} else {
@@ -92,21 +101,36 @@ export class AdminEditProductComponent implements OnInit {
 				name: this.product.name,
 				brand: this.product.brand,
 				imageURL: this.product.imageURL,
+				imagePath: this.product.imagePath,
 				price: this.product.price,
    				numInStock: this.product.numInStock,
 				description: this.product.description
 					   });
+			if (this.oldImage.length > 0) {
+				this.removeImage(this.oldImage);
+			}
 			alert('Product has been edited');
 			this.goBack();
 		}
 	}	
 	
   itemRemove(){
+	//Removes images, if an old image then delete
+	this.removeImage(this.product.imagePath);
+	if (this.oldImage.length > 0) {
+		this.removeImage(this.oldImage);
+	}
+	//Deletes database content for the product
     const itemsRef = this.db.object('/Products/'+this.key);
     itemsRef.remove();
 	alert('Product has been deleted');
 	this.goBack();
   }
+	
+	removeImage(pathToRemove:string){
+		const storageRef = firebase.storage().ref();
+   		storageRef.child(pathToRemove).delete();
+	}
 	
 	goBack(): void {
 		this.location.back();
